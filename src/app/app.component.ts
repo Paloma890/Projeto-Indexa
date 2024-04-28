577f4d397e5ad975d9bf0ad31595753da1d63e8c
unknown <palomaturettaa@gmail.com>
2024-04-27 21:28:44 -0300

    indexa

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormularioContatoComponent,
+    ListaContatosComponent,
+    RouterOutlet
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+}
---
+++ src/app/componentes/contato/contato.ts
@@ -0,0 +1,11 @@
+export interface Contato {
+    id: number;
+    nome: string;
+    avatar: string;
+    telefone: string;
+    email: string;
+    aniversario?: string;
+    redes?: string;
+    observacoes?: string;
+  }
+
